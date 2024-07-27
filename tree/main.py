4a8c66f924f2bd69b20918626022a24d9fc031b2
Ian Thompson <ianiat11@gmail.com>
2024-07-27 18:08:10 -0500

    modified:   _posts/2024-07-27-til-tree.md 	- Added new script showing how to hide private files/directories.
    modified:   tree/main.py
    	- Added flag to hide private files/directories by default.

--- tree/main.py
+++ tree/main.py
@@ -1,6 +1,7 @@
 """Contents of tree/main.py"""
 
 from pathlib import Path
+from typing import Annotated
 
 from rich import print
 from rich.tree import Tree
@@ -13,11 +14,20 @@ def main(
         directory: Path = typer.Argument(
             help="Directory path to represent as a tree."
         ),
+        all: Annotated[
+            bool,
+            typer.Option(
+                "--all/", "-a", help="Show all files."
+            ),
+        ] = False
 ) -> None:
     """Represent a path as a tree."""
     root = directory.cwd().as_posix()
     tree = Tree(root)
     for path in directory.iterdir():
+        if not all and path.name.startswith("."):
+            continue
+
         branch = path.as_posix()
         tree.add(branch)
 
